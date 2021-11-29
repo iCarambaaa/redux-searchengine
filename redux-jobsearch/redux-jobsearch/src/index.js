0b1e5aef8283569475926964bb09bf03892cdbd1
iCarambaaa <dkpress1916@zoho.eu>
2021-11-29 14:13:05 +0100

    persistance implemented

--- redux-jobsearch/redux-jobsearch/src/index.js
+++ redux-jobsearch/redux-jobsearch/src/index.js
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
-import configureStore from "./redux/store";
+import {configureStore, persistor} from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={configureStore}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+       <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- redux-jobsearch/redux-jobsearch/src/redux/store/index.js
+++ redux-jobsearch/redux-jobsearch/src/redux/store/index.js
@@ -3,7 +3,8 @@ import mainReducer from '../reducer'
 import favouritesReducer from '../reducer/favourites'
 import selectedJobReducer from '../reducer/selectedJob'
 import thunk from 'redux-thunk'                                         // redux-thunk is going to be helpful for handling async operations in the redux flow
-
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
 // we'll have two situations: the end user may have the redux devTools or not
 // if the devTools are installed in the user's browser, we'll have to use the compose function
@@ -22,7 +23,12 @@ export const initialState = {
 
 }
 
-const reducerGrande = combineReducers({                     // combine reducers here
+const persistConfig = {                                      // configure persistance here
+    key: 'root',
+    storage,
+  }
+
+const reducerGrande = combineReducers({                      // combine reducers here
     favourites: favouritesReducer,
     selectedJob: selectedJobReducer,
     jobList: mainReducer
@@ -30,10 +36,15 @@ const reducerGrande = combineReducers({                     // combine reducers 
 })
 
 
-const configureStore = createStore(                         // need to import this in index.js    <Provider store={configureStore}>   
-    reducerGrande,
+const persistedReducer = persistReducer(persistConfig, reducerGrande) // wrap the reducer with persistance
+
+
+const configureStore = createStore(                           // need to import this in index.js    <Provider store={configureStore}>   
+    persistedReducer,                                         // pass the persisted reducer instead of rootReducer to createStore
     initialState,
-    aComposeFunctionThatAlwaysWorks(applyMiddleware(thunk)) // composing two middlewares (DEV & thug) thug needs to be wrapped in applyMiddleware()
+    aComposeFunctionThatAlwaysWorks(applyMiddleware(thunk))   // composing two middlewares (DEV & thug) thug needs to be wrapped in applyMiddleware()
 )
 
-export default configureStore
+const persistor = persistStore(store)               // used to create the persisted store - need to import in index.js   <PersistGate loading={null} persistor={persistor}>
+
+export {configureStore, persistor}
